49824462a37ecd59b3f4a78e7b547abb1e73b001
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-28 19:28:56 +0000

    added function to control flow of all other functions

--- iss.js
+++ iss.js
@@ -16,7 +16,8 @@ const fetchMyIP = function(callback) {
 
     if (response.statusCode !== 200) {
       // if non-200 status, assume server error
-      return callback(Error(`Status code ${response.statusCode} when fetching IP. Response: ${body}`), null);
+      callback(Error(`Status code ${response.statusCode} when fetching IP. Response: ${body}`), null);
+      return;
     }
     const ip = JSON.parse(body).ip;
     callback(null, ip);
@@ -77,13 +78,43 @@ const fetchISSFlyOverTimes = function(coords, callback) {
       return;
     }
 
-    let flyoverTime = JSON.parse(body).response;
-    console.log(flyoverTime);
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
   });
-
  
 
 };
 
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results. 
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */ 
+ const nextISSTimesForMyLocation = function(callback) {
+   fetchMyIP((error, ip) => {
+     if (error) {
+       return callback(error, null);
+     }
+     fetchCoordsByIP(ip, (error, loc) => {
+       if (error) {
+         return callback(error, null);
+       }
+       fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+         if (error) {
+           return callback(error, null);
+         }
+         callback(null, nextPasses);
+       });
+     });
+   }); 
+};
+
+// Only export nextISSTimesForMyLocation and not the other three (API request) functions.
+// This is because they are not needed by external modules.
+
 //fetchMyIP();
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes};
+module.exports = { nextISSTimesForMyLocation };
